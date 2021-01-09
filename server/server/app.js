4206ab1152534f97d4d91ec3f1e1cad8542ae39d
Dmitriy Ivanov <dmitriy.p86@ya.ru>
2021-01-09 17:24:15 +0600

    frontend-core-created

--- server/server/app.js
+++ server/server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('../schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
@@ -12,6 +13,8 @@ mongoose.connect(process.env.DB_CONNECTION_ADDRESS, {
    useUnifiedTopology: true,
    useFindAndModify: false
 })
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
    schema,
